d63cc38e276083600d6e80f5e5cd1f827c319b35
Daniel-Martinez24 <dan24diaz@gmail.com>
2018-09-30 22:59:00 -0500

    Primer dia.
    Este fue el esquema y lo ocupado durante el primer dia del curso.

---
+++ Day one/hola_mundo.py
@@ -0,0 +1,23 @@
+#Print es la funcion que se utiliza para imprimir en pantalla un mensaje, entre comillas van
+#los parametros, es decir, lo que quieres que imprima, y no hay necesidad de ";"
+#como en otros lenguajes
+print("Hola mundo")
+
+#Declarando variablea INT es decir un valores numericos.
+numeromayor = 100
+numeromenor =  1
+
+#Hacer una suma en Python es super facil.
+suma = numeromayor + numeromenor
+print("La suma de 100 + 1 es :" + str(suma))
+
+#Para hacer cuentas matematicas, los simbolos, son los mismo que se ocuparian normalmente.
+mitad= numeromayor / 2
+print("la mitad del numero mayor es: "+ str(mitad))
+
+total =suma*mitad
+
+print("la suma de los numeros naturales del 1 al 100 es igual a: " + str(total))
+
+mul = 3 * "hola"
+print(mul)
---
+++ README.md
@@ -0,0 +1,6 @@
+Este esta es la guia de lo que se impartio en el ITVer, durante un curso de introduccion a la programacion.
+el lenguaje utilizado es Python, lo comparto para aquellos quienes no pudieron asistir a alguna clase,
+para futuras generacion IEEE principalmente computer society itver, para tener un registro,
+o bien quien tenga el deseo de aprender.
+
+Es un curso basico, te recomiendo complementar lo aprendido aqui con otras fuentes de aprendizaje.
